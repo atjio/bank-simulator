e22c77d35d2ddcdcbb99cd3c98979d47dca78999
KenToshi <anthony.tjio@outlook.co.id>
2019-05-10 23:12:46 +0700

    Add index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def current_user # For simplicity purpose assume the user have logged in
+		User.find_by(id: 1) 
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ApplicationRecord
+	require 'xfers'
+
 	has_one :user_account
 	has_many :virtual_accounts
+
+	def balance 
+		user = self.xfers_user
+		return user[:available_balance] if !user.nil?
+	end
+
+	def virtual_accounts
+		user = self.xfers_user
+		return user[:bank_accounts] if !user.nil?
+	end
+
+	def xfers_user
+		begin 
+			Xfers.set_api_key(self.xfers_api_key)
+			Xfers.set_id_sandbox
+			return Xfers::User.retrieve
+		rescue Xfers::XfersError => e
+  			puts "Exception: " + e.to_s # Let's use this for logging for now
+		end 
+	end
 end
